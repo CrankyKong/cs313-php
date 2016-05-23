b5803f57611492e0acafc96d52167884d194a723
Logan Skidmore <Logan Skidmore>
2016-05-23 16:09:05 -0600

    added nav bar to these sites

--- rpgtube/profile.php
+++ rpgtube/profile.php
@@ -72,33 +72,44 @@
         <li><a href="index.php#home">HOME</a></li>
 		<li><a href="comingsoon.html">ABOUT RPGTUBE</a></li>
         <li><a href="comingsoon.html">VIDEOS</a></li>
-        <li class="dropdown">
+        
+		<?php
+		//Profile for session
+		if($_SESSION['user_id'] != 42){
+		echo '<li class="dropdown">
           <a class="dropdown-toggle" data-toggle="dropdown" href="#">Profile
           <span class="caret"></span></a>
           <ul class="dropdown-menu">
-            <li><a href="comingsoon.html">Account Info</a></li>
-            <li><a href="comingsoon.html">Character</a></li>
-            <li><a href="comingsoon.html">Settings</a></li> 
+            <li><a href="profile.php">Character</a></li>
+            <li><a href="profile.php#inventory">Inventory</a></li>
+			<li><a href="profile.php#friends">Friends</a></li>
+            <li><a href="profile.php#accountInfo">Account Info</a></li> 
+			<li><a href="signout.php">Sign out!</a></li> 
           </ul>
-        </li>
-		<li><a href="/users/sign_up">Sign Up</a></li>
+        </li>';
+		}
+		else
+		{
+		//Sing in or up
+		echo '
+		<li><a href="comingsoon.html">Sign Up</a></li>
           <li class="divider-vertical"></li>
           <li class="dropdown">
             <a class="dropdown-toggle" href="#" data-toggle="dropdown">Sign In <strong class="caret"></strong></a>
             <div class="dropdown-menu" style="padding: 15px; padding-bottom: 0px;">
-              <!-- Login form here -->
-				<form action="[YOUR ACTION]" method="post" accept-charset="UTF-8">
+				<form action="signin.php" method="post" accept-charset="UTF-8">
 				<input id="user_username" style="margin-bottom: 15px;" type="text" name="user[username]" size="30" />
 				<input id="user_password" style="margin-bottom: 15px;" type="password" name="user[password]" size="30" />
 				<input id="user_remember_me" style="float: left; margin-right: 10px;" type="checkbox" name="user[remember_me]" value="1" />
-				<label class="string optional" for="user_remember_me"> Remember me</label>
- 
+				<label class="string optional" for="user_remember_me"> Remember me</label> 
 				<input class="btn btn-primary" style="clear: left; width: 100%; height: 32px; font-size: 13px;" type="submit" name="commit" value="Sign In" />
 				<br />
 				<br />
 				</form>
             </div>
-          </li>
+          </li>';
+		}
+		  ?>
         <li><a href="#"><span class="glyphicon glyphicon-search"></span></a></li>
       </ul>
     </div>
